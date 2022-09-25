388acaeffdf296a41c55fdfaaab483dbb663afe6
avramstefan <94166855+avramstefan@users.noreply.github.com>
2022-09-25 20:38:20 +0300

    Delete lab_01 directory

--- lab_01/task_4/task04.py
+++
@@ -1,9 +0,0 @@
-def func(given_numbers):
-    ################### TO DO #########################
-
-    # Completeaza doar linia urmatoare
-    nice_list = []
-
-    ###################################################
-
-    return nice_list
